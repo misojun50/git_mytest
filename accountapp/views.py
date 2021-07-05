a3800e3e85fdb1fef9f490a1efcce8c756ee0914
Jun <misojun50@gmail.com>
2021-07-05 12:16:21 +0900

    7/5 commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,3 +5,4 @@ from django.shortcuts import render
 
 def hello_world(request):
     return render(request, 'accountapp/hello_world.html')
+# 굳이 templates를 새로 만들고 안에 accountapp폴더를 만들고 html 생성 이유 : 그냥 html만 있으면 경로 꼬임 가능성
