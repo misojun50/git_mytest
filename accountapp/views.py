b3c699b5a5f8d21a1a993959c958765b3cf3b283
Jun <misojun50@gmail.com>
2021-07-19 12:16:40 +0900

    7/19 commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.models import HelloWorld
 
@@ -59,3 +59,10 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     #지금은 detail이 안됨. <int:pk>를 지정안했기 때문.
     template_name = 'accountapp/update.html'
+
+#회원탈퇴 기능
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_User'
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = 'accountapp/delete.html'
