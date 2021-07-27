a5881cccf91e9f6bcfacadf6f0afa3700431fab8
Jun <misojun50@gmail.com>
2021-07-27 18:04:06 +0900

    7/27 commit4

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreationView
+from profileapp.views import ProfileCreationView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
     # as_view 잊지 말것.
-    path('create/', ProfileCreationView.as_view(), name='create')
+    # create 는 없는걸 만드는거라 상관없지만 update 는 있는걸 수정하는거기에 int:pk 지정
+    path('create/', ProfileCreationView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -18,6 +18,13 @@ class ProfileCreationView(CreateView):
         # 검증과정이 끝나고 나서 실행되는 함수.(성공했을 때)
         form.instance.user = self.request.user
         return super().form_valid(form)# 부모의 form_valid값을 불러옴
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
 
     #데이터베이스 안에는 이미지 자체가 저장되지 않음. 정확히는 경로를 저장함
     #그리고 media,profile 폴더가 만들어진 이유 : media_root 를 설정해 뒀기 때문.
