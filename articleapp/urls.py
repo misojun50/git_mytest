c2a7fc1576dfabbb46fdddd25f945f63947b1ea9
Jun <misojun50@gmail.com>
2021-08-03 17:16:40 +0900

    8/3 commit2

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
     path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    #detail은 어떤 게시글을 수정, 삭제 등을 해야 하므로
+
 
 
 ]
