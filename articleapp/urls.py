4dc156458bc4617c4afa3a9445275973b7fb6dbb
Jun <misojun50@gmail.com>
2021-08-03 17:51:03 +0900

    8/3 commit3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articleapp.views import ArticleCreateView, ArticleDetailView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
 
 app_name = 'articleapp'
 
@@ -10,6 +10,9 @@ urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     #detail은 어떤 게시글을 수정, 삭제 등을 해야 하므로
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+
+
 
 
 
