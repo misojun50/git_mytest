6bab4f55fcff8d2b074d56ccbdf74f8df6ab240d
Jun <misojun50@gmail.com>
2021-08-10 17:13:36 +0900

    8/10 commit2

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView
 
+from commentapp.decorators import comment_owner_var
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
@@ -22,6 +26,8 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('articleapp:detail', kwargs={'pk':self.object.article.pk})
 
+@method_decorator(comment_owner_var, 'get')
+@method_decorator(comment_owner_var, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
