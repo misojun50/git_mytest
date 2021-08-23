51015c71950140e2a8dc727629c744749ddafeb9
Jun <misojun50@gmail.com>
2021-08-23 11:23:26 +0900

    8/23 commit1

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+@method_decorator(login_required, 'get')
+#@method_decorator(login_required, 'post')
+class SubscriptionView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        project = Project.objects.get(pk=kwargs['project_pk'])
+        subscription = Subscription.objects.filter(user=user, project=project)
+        # 좌측 = 모델만들때 속성, 우측 = 설정한 속성
+
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user, project=project).save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk':kwargs['project_pk']})
